ccb4e79e95d57c0ec294d3e414185373a3279a26
mysaas2024 <mysaas2024gmail.com>
2026-02-12 09:06:08 +0500

    first commit

---
+++ NumPy-Starting/NumPy-Starting.py
@@ -0,0 +1,11 @@
+#numpy works with array 
+import numpy as np
+#creating an ndarry
+arr=np.array([1,2,45,6,32,2])
+print(type(arr))
+
+#there aree many arrays such as 0-d and 1-d 
+#the function ndim
+
+a=np.array([23,5234,23423,423,423,4,2],[2354235235235],[2345235,23523523,5235])
+print(a.ndim)
