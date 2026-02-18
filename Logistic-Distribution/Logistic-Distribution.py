028c87e0bb06b12f68d88981ff4d41fd63d798fd
mysaas2024 <mysaas2024gmail.com>
2026-02-18 18:35:25 +0500

    sdsd

--- Logistic-Distribution/Logistic-Distribution.py
+++ Logistic-Distribution/Logistic-Distribution.py
@@ -5,3 +5,7 @@ import seaborn as sns
 
 sns.displot(random.logistic(size=1000),kind="kde")
 plt.show()
+x=np.array(random.normal(loc=23, scale=0.4 ,size=3))
+y=np.array(random.normal(loc=23, scale=0.6 ,size=3))
+plt.scatter(x,y)
+plt.show()
